6dc50a04cebbb4fe07a923df6656142086ffac42
mohsen <mohsenbarzegar94@gmail.com>
2016-11-27 12:51:10 +0330

    Create .m file

---
+++ HW3_TA.m
@@ -0,0 +1,10 @@
+%% ------------------ In the name of GOD ------------------
+%   title  : HW_3_TA (Multi media)
+%            "bale" & "kheyr" recognition
+%   author : mohsen barzegar
+%   student num :9223014
+%   email  : mohsenbarzegar94@gmail.com
+
+clear all ; clc ; close all ;
+
+%% imadjust
